1073d187477ded2ca63384d2323e9d0707b17a8e
NovweraAmjad <novweraamjad@gmail.com>
2026-07-16 14:50:03 +0500

    Implemented NavMesh ghost AI

---
+++ Assets/Scripts/GhostAI.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GhostNavAI : MonoBehaviour
+{
+    public Transform player;
+
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (player != null)
+        {
+            agent.SetDestination(player.position);
+        }
+    }
+}
